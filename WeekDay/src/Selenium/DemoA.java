15f5aba5dc602454217006b16a79f2481c21fead
Kumar <Kumar@Kumar-PC>
2018-03-23 00:56:35 -0700

    Hello

--- WeekDay/src/Selenium/DemoA.java
+++ WeekDay/src/Selenium/DemoA.java
@@ -11,7 +11,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class DemoA {
 	static {
 System.setProperty("webdriver.chrome.driver","./driver/chromedriver.exe");
-
 }
 	public static void main(String[] args) {
 		WebDriver driver=new ChromeDriver();
