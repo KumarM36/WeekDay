7155178ab12f01a0005ede549cda6272656413e9
Kumar <Kumar@Kumar-PC>
2018-03-23 01:39:01 -0700

    Hi

--- WeekDay/src/Selenium/DemoA.java
+++ WeekDay/src/Selenium/DemoA.java
@@ -11,7 +11,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class DemoA {
 	static {
 System.setProperty("webdriver.chrome.driver","./driver/chromedriver.exe");
-}
+} 
 	public static void main(String[] args) {
 		WebDriver driver=new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
